dfe3cb8f8c20cca69aa389d5799ebc801c22e5fd
RoryMurphy1997 <43140750+RoryMurphy1997@users.noreply.github.com>
2018-11-07 16:59:17 +0000

    Work on interrogating
    Successfully pulled my own repository for this module and created an error handle on the loop for if it receives an empty dataframe

--- Interrogate API.R
+++ Interrogate API.R
@@ -1,14 +1,16 @@
+#Run these 3 lines once to install necessary packages
 install.packages("jsonlite")
-library(jsonlite)
 install.packages("httpuv")
-library(httpuv)
 install.packages("httr")
+
+#Run these 3 lines the first time code is run
+library(jsonlite)
+library(httpuv)
 library(httr)
 
-# Can be github, linkedin etc depending on application
+# Run from here each time
 oauth_endpoints("github")
 
-# Change based on what you 
 myapp <- oauth_app(appname = "Software_Engineering_GitHub_Assignment",
                    key = "9cdd2af0684fe05044a7",
                    secret = "a58134728046024f716360e2b7bf4c97210e38fe")
@@ -18,7 +20,7 @@ github_token <- oauth2.0_token(oauth_endpoints("github"), myapp)
 
 # Use API to find users
 gtoken <- config(token = github_token)
-req <- GET("https://api.github.com/repositories", gtoken)
+req <- GET("https://api.github.com/users/RoryMurphy1997/repos", gtoken)
 
 # Take action on http error
 stop_for_status(req)
@@ -28,11 +30,17 @@ json1 = content(req)
 
 # Convert to a data.frame
 gitReposDF = jsonlite::fromJSON(jsonlite::toJSON(json1))
-#Iterate through and get all
-for(i in 1:nrow(gitReposDF))
+#Gives null for dataframe with one item, need to fix
+nrow(gitReposDF)
+if(is.null(nrow(gitReposDF)))
 {
-  print(gitReposDF[i,1])
+  print("Repository is empty")
+}else{
+  for(i in 1:nrow(gitReposDF))
+  {
+    print(gitReposDF$full_name)
+  }
 }
 
 # Subset data.frame
-gitDF[gitReposDF$full_name == "jtleek/datasharing", "created_at"] 
+gitReposDF[gitReposDF$full_name == "jtleek/datasharing", "created_at"] 
